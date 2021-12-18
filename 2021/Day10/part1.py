e8a904b9b1b966c2f02dc53ff3601deec1c6bc95
Barnabás Mórocz <mester3210@gmail.com>
2021-12-18 18:44:32 +0100

    Day10 part1

---
+++ 2021/Day10/part1.py
@@ -0,0 +1,33 @@
+import re
+
+pairs = {'(':')','[':']','{':'}','<':'>'}
+points = {')' : 3,']': 57,'}' : 1197,'>' : 25137}
+
+count = 0
+with open("input.txt") as file:
+    for line in file:       
+        prevLength = len(line)
+        while True:
+            
+            line = re.sub(r"\(\)","",line)
+            line = re.sub(r"\[\]","",line)
+            line = re.sub(r"\{\}","",line)
+            line = re.sub(r"<>","",line)
+
+            if prevLength == len(line):
+                break
+            prevLength = len(line)           
+
+        for i in range(len(line)-1):
+            if line[i] not in ['(','[','{','<']:
+                continue
+            if line[i+1] not in [')',']','}','>']:
+                continue
+            
+            bracket = line[i]
+            if line[i+1] != pairs[bracket]:
+                count += points[line[i+1]]
+                break
+            
+print(count)
+
